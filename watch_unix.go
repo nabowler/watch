ccc86ed9f70b368db1ffcb1dd3c7caa5e366b6c3
Nathan Bowler <nzb301@psu.edu>
2019-11-27 17:42:03 -0500

    feature(unix): Set defaultshell to /usr/bin/zsh

--- watch_unix.go
+++ watch_unix.go
@@ -12,7 +12,22 @@ import (
 	"github.com/kr/pty"
 )
 
-const defaultShell = "bash -cli"
+var defaultShell string
+
+func init() {
+	sh := os.Getenv("SHELL")
+	if sh == "" {
+		sh = "bash"
+	}
+	// TODO: detect running shell. This is just the default shell.
+	// `ps -p $$ -oargs=`, though this likely gives this program, so we'd need the parent
+	// Also may which to try to run `$SHELL -cli echo test` and check for an error
+
+	// I checked the man page and/or tested ash, bash, csh, dash, ksh, fish, tcsh, zsh
+	// The man pages all listed -cli, except ash and zsh which don't specify -l, but
+	// still support it as far as I can tell
+	defaultShell = sh + " -cli"
+}
 
 func cmdOutput(cmd *exec.Cmd, buf *bytes.Buffer) error {
 	ptmx, err := pty.Start(cmd)
